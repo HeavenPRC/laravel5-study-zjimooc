015cb7d31ce7bf00e6590f134ca81cd101ec300a
jian.zhang <jian.zhang@oda-group.com>
2017-12-25 13:30:35 +0800

    调整完毕

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Student;
+
 class StudentController extends Controller
 {
 
 	public function index()
 	{
-
-		return view('student.index');
+        $students=Student::all();
+        //dd($students);
+		return view('student.index', [
+            'students' => $students
+		]);
 	}
 }
---
+++ app/Student.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    protected $table = 'student';
+
+    public $timestamps = true;
+
+    protected $primaryKey = 'id';
+
+    protected function getDateFormat()
+    {
+   	   return time();
+    }
+
+    protected function asDateTime($val)
+    {
+    	return $val;
+    }
+
+}
